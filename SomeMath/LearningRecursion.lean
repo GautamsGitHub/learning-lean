092d549b1d5f666e2da2feb287ae98f86fd07428
Gautam <1gautamj@gmail.com>
2025-04-02 12:19:56 +0100

    proof that reverse (reverse xs) = xs

--- SomeMath/LearningRecursion.lean
+++ SomeMath/LearningRecursion.lean
@@ -42,50 +42,24 @@ theorem reverse_maintains_length {α : Type u} (as : List α)
   rw [reverse, rloop_maintains_length as empty, len]
   simp
 
-theorem reverse_reverse_loop {α : Type u} (xs rts : List α)
-  : reverse.loop xs rts = reverse.loop (reverse.loop rts xs) empty := by
-  match xs, rts with
-  |
+theorem reverse_loop_swap {α : Type u} (xs ys : List α)
+  : reverse.loop (reverse.loop xs ys) empty
+    = reverse.loop (reverse.loop empty ys) xs := by
+  match xs, ys with
+  | empty, ys => simp [reverse.loop]
+  | cons x xs, ys =>
+    rw [
+      reverse.loop,
+      reverse.loop,
+      reverse_loop_swap,
+      reverse.loop,
+      reverse.loop]
 
-theorem reverse_reverse {α : Type u} (as : List α)
-  : as = reverse (reverse as) := by
-  have loopstarter (as : List α)
-    : as = reverse.loop empty as := rfl
-  have rrloop (as ras : List α)
-    : reverse.loop as ras = reverse (reverse.loop ras as) :=
-    List.recOn as
-    (List.recOn ras (by simp [reverse.loop, reverse]) (by
-      intros a as1 ih
-      admit
-    ))
-    (by admit)
-  calc as
-    _ = reverse.loop empty as := loopstarter as
-    _ = reverse (reverse.loop as empty) := rrloop empty as
-    _ = reverse (reverse as) := by
-      rw [rrloop as empty, ← loopstarter as]
-
-theorem reverse_reverse' {α : Type u} (as : List α)
-  : as = reverse (reverse as) := by
-  have loopstarter (as : List α)
-    : reverse.loop empty as = as := rfl
-  have rrloop (as : List α)
-    : reverse (reverse.loop as empty) = as :=
-    List.recOn as
-    (by simp [reverse.loop, reverse])
-    (by
-      intros a as h1
-      rw [reverse.loop]
-      admit
-    )
-  calc as
-    _ = reverse (reverse.loop as empty) := by simp [rrloop]
-    _ = reverse (reverse as) := by rw [← reverse]
-
-
-variable (C : Nat → Type u)
-variable (α : Type)
-variable (r : Prop)
-
-#check List.below
-#reduce List.below (α := Nat) (motive := fun l => len l = len l) (cons 3 empty)
+theorem reverse_reverse {α : Type u} (xs : List α)
+  : xs = reverse (reverse xs) := by
+  rw [
+    reverse,
+    reverse,
+    reverse_loop_swap,
+    reverse.loop,
+    reverse.loop]
