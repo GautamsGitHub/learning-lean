20853387ab6095c92c06ac9110c43a3ad71e1a90
Gautam <1gautamj@gmail.com>
2025-04-07 18:19:43 +0100

    polyhedron and dual polyhedron

--- SomeMath/Simplex.lean
+++ SomeMath/Simplex.lean
@@ -3,13 +3,25 @@ import Mathlib.Data.Fin.VecNotation
 import Mathlib.Data.Real.Basic
 
 
-def elwileq {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
-  ∀ i : Fin n, v1 i ≤ v2 i
+def elwiL {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
+  ∀ i : Fin n, v1 i < v2 i
 
+def elwiG {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
+  ∀ i : Fin n, v1 i > v2 i
+
+def elwiEq {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
+  ∀ i : Fin n, v1 i = v2 i
 
 def polyhedron {n m : Nat}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real)
   (x : Fin n → Real)
   : Prop :=
-  elwileq (Matrix.mulVec A x) b
+  ¬ elwiL (Matrix.mulVec A x) b
+
+def dual_polyhedron {n m : Nat}
+  (A : Matrix (Fin m) (Fin n) Real)
+  (c : Fin n → Real)
+  (u : Fin m → Real)
+  : Prop :=
+  (elwiEq (Matrix.mulVec (A.transpose) u) c) ∧ (¬ elwiL u 0)
