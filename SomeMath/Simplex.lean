203613719240e2d2056bdd6c1098d241fba558d1
Gautam <1gautamj@gmail.com>
2025-04-11 14:00:00 +0100

    define feasible basis. but inverses will need revisit

--- SomeMath/Simplex.lean
+++ SomeMath/Simplex.lean
@@ -1,6 +1,7 @@
 import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Fin.VecNotation
 import Mathlib.Data.Real.Basic
+import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 
 
 def elwiL {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
@@ -12,14 +13,14 @@ def elwiG {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
 def elwiEq {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
   ∀ i : Fin n, v1 i = v2 i
 
-def polyhedron {n m : Nat}
+def polyhedron {m n : Nat}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real)
   (x : Fin n → Real)
   : Prop :=
   ¬ elwiL (Matrix.mulVec A x) b
 
-def dual_polyhedron {n m : Nat}
+def dual_polyhedron {m n : Nat}
   (A : Matrix (Fin m) (Fin n) Real)
   (c : Fin n → Real)
   (u : Fin m → Real)
@@ -27,22 +28,13 @@ def dual_polyhedron {n m : Nat}
   (elwiEq (Matrix.mulVec (A.transpose) u) c) ∧ (¬ elwiL u 0)
 
 
-structure Prebasis {m : Nat} where
-  n : Fin m
+structure Prebasis {m : Nat} (n : Fin m) where
   f : (Fin n) → (Fin m)
+  inj : Function.Injective f
 
 structure Prebasis' {m : Nat} {n : Fin m} where
   set : Finset (Fin m)
   hsize : set.card = n
-
-
-#check @Prebasis'.mk 3 1 (Finset.cons
-    (@Fin.mk 8 6 (by simp))
-    (@Finset.empty (Fin 3))
-    (Finset.not_mem_empty _))
-  (by
-    simp
-    rfl)
 
 noncomputable def row_submx' {m : Nat}
   (p : Nat)
@@ -61,8 +53,28 @@ def row_submx {m p s : Nat}
     Q (I i) j
   )
 
-def matrix_of_prebasis {m p : Nat}
+def matrix_of_prebasis {m p : Nat} {n : Fin m}
   (Q : Matrix (Fin m) (Fin p) Real)
-  (I : @Prebasis m)
-  : (Matrix (Fin I.n) (Fin p) Real)
+  (I : @Prebasis m n)
+  : (Matrix (Fin n) (Fin p) Real)
   := row_submx Q I.f
+
+structure MyBasis {m : Nat} {n : Fin m}
+  (A : Matrix (Fin m) (Fin n) Real) where
+  I : @Prebasis m n
+  bas : (matrix_of_prebasis A I).det = 0
+
+noncomputable def my_basis_point {m : Nat} {n : Fin m}
+  (A : Matrix (Fin m) (Fin n) Real)
+  (b : Fin m → Real)
+  (Ib : MyBasis A)
+  : (Fin n → Real)
+  := Matrix.mulVec
+    (Ring.inverse (matrix_of_prebasis A Ib.I))
+    (b ∘ Ib.I.f)
+
+structure FeasibleBasis {m : Nat} {n : Fin m}
+  (A : Matrix (Fin m) (Fin n) Real)
+  (b : Fin m → Real) where
+  Ib : @MyBasis m n A
+  inph : polyhedron A b (my_basis_point A b Ib)
