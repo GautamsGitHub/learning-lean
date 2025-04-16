664cc7f944299bae09dcf3af23f176cd7db5926f
Gautam <1gautamj@gmail.com>
2025-04-16 09:13:52 +0100

    weak duality

--- SomeMath/Simplex.lean
+++ SomeMath/Simplex.lean
@@ -4,6 +4,7 @@ import Mathlib.Data.Real.Basic
 import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
 import Mathlib.Algebra.Module.LinearMap.Defs
 import Mathlib.LinearAlgebra.Matrix.ToLin
+import Mathlib.LinearAlgebra.Matrix.DotProduct
 
 
 def elwiL {n : Nat} (v1 v2 : Fin n → Real) : Prop :=
@@ -20,14 +21,15 @@ def polyhedron {m n : Nat}
   (b : Fin m → Real)
   (x : Fin n → Real)
   : Prop :=
-  ¬ elwiL (Matrix.mulVec A x) b
+  (Matrix.mulVec A x) ≥ b
 
 def dual_polyhedron {m n : Nat}
   (A : Matrix (Fin m) (Fin n) Real)
   (c : Fin n → Real)
   (u : Fin m → Real)
   : Prop :=
-  (elwiEq (Matrix.mulVec (A.transpose) u) c) ∧ (¬ elwiL u 0)
+  ((Matrix.mulVec (A.transpose) u) = c) ∧ (u ≥ 0)
+  -- (elwiEq (Matrix.mulVec (A.transpose) u) c) ∧ (¬ elwiL u 0)
 
 
 structure Prebasis {m : Nat} (n : Fin m) where
@@ -61,23 +63,23 @@ def matrix_of_prebasis {m p : Nat} {n : Fin m}
   : (Matrix (Fin n) (Fin p) Real)
   := row_submx Q I.f
 
-structure MyBasis {m : Nat} {n : Fin m}
+structure IBasis {m : Nat} {n : Fin m}
   (A : Matrix (Fin m) (Fin n) Real) where
   I : @Prebasis m n
   bas : (matrix_of_prebasis A I).det = 0
+  -- or something meaning matrix is invertible
 
-noncomputable def my_basis_point {m : Nat} {n : Fin m}
+noncomputable def i_basis_point {m : Nat} {n : Fin m}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real)
-  (Ib : MyBasis A)
+  (Ib : IBasis A)
   : (Fin n → Real)
   := Matrix.mulVec
     (Ring.inverse (matrix_of_prebasis A Ib.I))
     (b ∘ Ib.I.f)
 
 structure PrebasisWithBasis {m : Nat} {n : Fin m}
-  (A : Matrix (Fin m) (Fin n) Real)
-  (b : Fin m → Real) where
+  (A : Matrix (Fin m) (Fin n) Real) where
   I : @Prebasis m n
   basis : Basis (Fin n) Real (Fin n → Real)
   -- h : the basis is made of the I vectors from A
@@ -85,21 +87,48 @@ structure PrebasisWithBasis {m : Nat} {n : Fin m}
 def basis_point {m : Nat} {n : Fin m}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real)
-  (Ib : PrebasisWithBasis A b)
+  (Ib : PrebasisWithBasis A)
   : (Fin n → Real)
   := (Ib.basis).repr (b ∘ Ib.I.f)
 
-structure FeasibleBasisMy {m : Nat} {n : Fin m}
+structure FeasibleBasisI {m : Nat} {n : Fin m}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real) where
-  Ib : @MyBasis m n A
-  inph : polyhedron A b (my_basis_point A b Ib)
+  Ib : @IBasis m n A
+  inph : polyhedron A b (i_basis_point A b Ib)
 
 structure FeasibleBasis {m : Nat} {n : Fin m}
   (A : Matrix (Fin m) (Fin n) Real)
   (b : Fin m → Real) where
-  Ib : PrebasisWithBasis A b
+  Ib : PrebasisWithBasis A
   inph : polyhedron A b (basis_point A b Ib)
 
-#check Basis.ofEquivFun
-#check LinearEquiv.ofBijective
+
+noncomputable def reduced_cost_of_basis {m : Nat} {n : Fin m}
+  (A : Matrix (Fin m) (Fin n) Real)
+  (c : Fin n → Real)
+  (Ib : @IBasis m n A)
+  : (Fin n → Real)
+  := Matrix.mulVec
+    (Ring.inverse (matrix_of_prebasis A Ib.I).transpose)
+    c
+
+
+theorem weak_duality {m : Nat} {n : Fin m}
+  (A : Matrix (Fin m) (Fin n) Real)
+  (b : Fin m → Real)
+  (c : Fin n → Real)
+  : ∀ x : Fin n → Real, ∀ u : Fin m → Real,
+    (polyhedron A b x) ∧ (dual_polyhedron A c u) →
+    (dotProduct c x) ≥ (dotProduct b u)
+  := by
+    intros x u h1
+    rcases h1 with ⟨ph, dph⟩
+    rcases dph with ⟨h2, h3⟩
+    rw [
+      ← h2,
+      Matrix.mulVec_transpose,
+      ← Matrix.dotProduct_mulVec,
+      dotProduct_comm]
+    apply LE.le.ge
+    apply dotProduct_le_dotProduct_of_nonneg_right ph (LE.le.ge h3)
